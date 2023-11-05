f3a60b6b82fa4047ec633c856b6ba75b4b8a972c
Oleg Schegol <olschegolev@yahoo.com>
2023-11-05 20:40:10 +0500

    add task5 file to search array

--- src/task5/App.java
+++ src/task5/App.java
@@ -1,37 +1,16 @@
 package task5;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
         Corporation corporation = new Corporation();
-        /*corporation = FakeDate.date();
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/task5/corporation.dat")))
-        {
-
-            oos.writeObject(corporation);
-            System.out.println("File has been written");
-        }
-        catch(Exception ex){
-
-            System.out.println(ex.getMessage());
-        }*/
-
-        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src/task5/corporation.dat")))
-        {
-
-            corporation=(Corporation) ois.readObject();
-
-        }
-        catch(Exception ex){
-
-            System.out.println(ex.getMessage());
-        }
-        System.out.println(corporation.getEmployees());
-
+        corporation = FakeDate.date();
+        ArrayList<Employee> save = new ArrayList<>();
+        save = corporation.searchAllEmployeeByName("Petrov",(s, l, a) -> s.getFirstName().equals(l));
+        Corporation.searchArrayWritingToFile("src/task5/search.dat", save);
+        ArrayList<Employee> e = new ArrayList<>();
+        e = Corporation.readingFromFile("src/task5/search.dat");
+        System.out.println(e);
     }
 }
